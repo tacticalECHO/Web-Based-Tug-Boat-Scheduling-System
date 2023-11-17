fe8e301909e5c920a2351e2bf7b45c620d76c7d1
Wenhao WU <scyww6@nottingham.edu.cn>
2023-11-17 15:05:34 +0800

    "example for web development"

--- web/WBTBSsystem/system/views.py
+++ web/WBTBSsystem/system/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the system index.")
+def team(request):
+    return render(request, 'index.html')
