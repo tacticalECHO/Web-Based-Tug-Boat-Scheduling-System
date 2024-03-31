15f681a000529ebcb4ae28340a63a9bf2c2f3e15
Shiting YAO <92799058+Jiahui000@users.noreply.github.com>
2024-03-31 11:45:27 +0800

    Add adminDashboard test

---
+++ website/NingboHarbour/cypress/e2e/adminDashboard_spec.cy.js
@@ -0,0 +1,74 @@
+describe('Administrator Dashboard Page Tests', () => {
+  before(() => {
+    cy.visit('http://127.0.0.1:8000/#/')
+    cy.get('input[type=text]').type('scyqd1')
+    cy.get('input[type=password]').type('dqr12345')
+    cy.get('.btn').click();
+    cy.get('#new-staff').click()
+    cy.get('#username').type('CP000D'); 
+    cy.get('#name').type('TestCaptain'); 
+    cy.get('#position').select('Captain');
+    cy.get('#confirm').click();
+  })
+
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:8000/#/')
+    cy.get('input[type=text]').type('scyqd1')
+    cy.get('input[type=password]').type('dqr12345')
+    cy.get('.btn').click();
+  });
+
+  it('Load page successfully', () => {
+    cy.url().should('include', '/admin')
+  })
+
+  it('Title bar is shown', () => {
+    cy.contains('h2', 'Your Dashboard').should('exist');
+  });
+
+  it('Captaion visible', () => {
+    cy.contains('div', 'Captain').should('exist');
+
+    cy.window().then(win => {
+      win.waiting = () => false;
+    });
+
+    cy.get('.table-container').should('be.visible');
+  });
+
+  it('Scheduler visible', () => {
+    cy.contains('div', 'Scheduler').should('exist');
+
+    cy.window().then(win => {
+      win.waiting = () => false;
+    });
+
+    cy.get('.table-container').should('be.visible');
+  });
+
+  it('Button new and delete is visible', () => {
+    cy.contains('button', 'New').should('exist')
+    cy.contains('button', 'Delete').should('exist')
+  })
+
+  it('Delete staff successfully', () => {
+    cy.get('#checkboxCP000D').check()
+    cy.get('#delete').click()
+
+    cy.on('window:confirm', (str) => {
+      expect(str).to.equal('Confirm to delete?')
+      return true
+    })
+
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Deleted successfully')
+    })
+
+    cy.get('#checkboxCP000D').should('not.exist')
+  })
+
+  it('Click new button redirect to New Staff page', () => {
+    cy.get('#new-staff').click()
+    cy.url().should('include', '/#/admin/add-new-staff')
+  })
+})
